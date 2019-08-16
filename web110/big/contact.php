5902deeaef52a07dcfa1b7554135e973beda7135
Melissa Guzman <52518338+mguzman11@users.noreply.github.com>
2019-08-15 23:22:30 -0700

    BIG website Final Project
    http://web-students.net/site43/web110/

---
+++ web110/big/contact.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+     <title>Melissa Guzman's Web Developer Examples and Resources</title>
+  <meta charset="utf-8" />
+  <meta name="robots" content="noindex,nofollow" />
+  <meta name="viewport" content="width=device-width" />
+  <link rel="stylesheet" href="css/big.css" />
+  <link rel="stylesheet" href="css/nav.css" />
+    <!--JQuery-->
+    <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+     <!--MenuMaker Plugin-->
+     <script src="https://s3.amazonaws.com/menumaker/menumaker.min.js" type="text/javascript"></script>
+     <!--Icon Library-->
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css"/>
+     <script src="js/script.js"></script>
+ </head>
+ <body>
+    <main>
+    <header>
+        <h1><a href="index.html">Melissa Guzman's: Web Developer Examples and Resources</a></h1>
+        <nav id="cssmenu">
+          <ul>
+             <li><a href="../index.html"><i class="fa fa-fw fa-institution"></i> WEB110</a></li>
+             <li><a href="index.html"><i class="fa fa-fw fa-home"></i> BIG Website</a></li>          
+              <!-- drop down menu for research pages -->
+              <li><a href="#"><i class="fa fa-fw fa-bars"></i> Research Topics</a>
+                <ul>
+                   <li><a href="accessibility.html"><i class="fa fa-fw fa-globe"></i> Accessibility</a></li>
+                   <li><a href="smo.html"><i class="fa fa-fw fa-thumbs-o-up"></i> Social Media Optimization</a></li>
+                   <li><a href="forms.html"><i class="fa fa-fw fa-desktop"></i> Forms</a></li>
+                </ul>
+             </li>
+             <li><a href="gallery.html"><i class="fa fa-fw fa-camera-retro"></i> Image Gallery</a></li>
+            <!-- drop down menu for Google Tools pages -->
+             <li><a href="#"><i class="fa fa-fw fa-bars"></i> Google Tools</a>
+                <ul>
+                   <li><a href="seo.html"><i class="fa fa-fw fa-desktop"></i> SEO</a></li>
+                   <li><a href="calendar.html"><i class="fa fa-fw fa-calendar"></i> Calendar</a></li>
+                   <li><a href="map.html"><i class="fa fa-fw fa-map-o"></i> Map</a></li>
+                </ul>
+             </li>
+             <li><a href="contact.php"><i class="fa fa-fw fa-server"></i> Contact Melissa</a></li>
+          </ul>
+        </nav>
+</header>
+        
+<body>
+    <main>
+     
+        <!-- START LEFT COLUMN -->
+        <section>
+            <h2 class="subheader">Contact Melissa</h2>
+                <?php include "includes/simple.php";?>
+<p class="clear-recaptcha"></p>
+        </section>
+        <!-- END LEFT COLUMN -->
+
+        <!-- START RIGHT COLUMN -->
+        <aside>
+            <h3>Contact Me</h3>
+                <ul>
+                    <li>Have questions about web design?</li>
+                    <li>Need a recommendation?</li>
+                    <li>Curious about my experience in the Programming Certificate program at Seattle Central College?</li>
+                </ul>
+        </aside>
+        <!-- END RIGHT COLUMN -->
+     
+    
+     <footer>
+      <p><small>&copy; 2019 by <a href="contact.php">Contact Melissa Guzman </a>, All Rights Reserved ~ <a href="http://validator.w3.org/check/referer" target="_blank">Valid HTML</a> ~ <a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a></small></p>
+    </footer>
+  </main>
+  
+     
+ </body>
